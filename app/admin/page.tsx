f97a775ccc7ef085267985f4beed927ca872b1e9
hasnain <raohasnaintofeeq@gmail.com>
2024-10-18 16:04:53 +0500

    added student form

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,37 +1,133 @@
 'use client'
-import React from "react";
-interface formProps {
-  name: string;
-  fname: string;
-}
-const formValues: formProps = {
-  name: "",
-  fname: "",
-}
-const handleState = () => {
-  const [value, setValue] = React.useState(formValues)
-  const [copy, setCopy] = React.useState([] as any)
-  const handleChange = (e: string, optional: string) => {
-    setValue((prev) => {
-      return {
-        ...prev,
-        [optional]: e
-      }
-    })
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { useRouter } from 'next/navigation';
+
+const drawerWidth = 240;
+const routes = [
+  {
+    name: "Add Student",
+    url: "/admin/addstudents",
+  },
+  {
+    name: "All Students",
+    url: "/admin/students",
+  },
+  {
+    name: "Attendance",
+    url: "/admin/Contact"
   }
-  const handleSubmit = () => {
-    setCopy((prev: []) => {
-      return [...prev, value]
-    })
-    setValue(formValues)
+]
+
+export default function DrawerAppBar(props: any) {
+  const router = useRouter();
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+  const handleClick = (url: string) => {
+    router.push(url)
   }
-  console.log("value ", copy)
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Online Attendane
+      </Typography>
+      <Divider />
+      <List>
+        {routes.map((item, index) => (
+          <ListItem key={index} disablePadding onClick={() => handleClick(item.url)}>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
   return (
-    <div>
-      <input type="text" value={value.name} onChange={(e) => handleChange(e.target.value, "name")} />
-      <input type="text" value={value.fname} onChange={(e) => handleChange(e.target.value, "fname")} />
-      <button type="submit" onClick={handleSubmit}>submit</button>
-    </div>
-  )
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav" position='fixed'>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, cursor: "pointer" }}
+            onClick={() => router.push("/admin")}
+          >
+            Online Attendance
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {routes.map((item, index) => (
+              <Button key={index} sx={{ color: '#fff' }} onClick={() => handleClick(item.url)}>
+                {item.name}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        {/* <Typography>
+          hasnain
+        </Typography> */}
+      </Box>
+    </Box>
+  );
 }
-export default handleState;
+
+// DrawerAppBar.propTypes = {
+//   /**
+//    * Injected by the documentation to work in an iframe.
+//    * You won't need it on your project.
+//    */
+//   window: PropTypes.func,
+// };
+
+// export default DrawerAppBar;
